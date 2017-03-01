78fef8d02e8f18427a2d4af050e07c67479bfb80
zhanghongbo <zhanghb@mmwipo.com>
2017-03-01 16:10:38 +0800

    create project

---
+++ TRZXPersonalProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZXPersonalProfile
+//
+//  Created by Rhino on 2017/3/1.
+//  Copyright © 2017年 Rhino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRZXPersonalProfile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRZXPersonalProfile
+//
+//  Created by Rhino on 2017/3/1.
+//  Copyright © 2017年 Rhino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
